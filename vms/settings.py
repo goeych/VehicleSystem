1c9d9afe936a1381a36db9aa6deb874dc4ef039d
goeych <110361567+goeych@users.noreply.github.com>
2023-07-03 17:14:13 +0800

    Add files via upload

--- vms/settings.py
+++ vms/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'generalExpense.apps.GeneralexpenseConfig',
     'accounts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Singapore'
 
 USE_I18N = True
 
